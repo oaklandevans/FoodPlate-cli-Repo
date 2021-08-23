3fb51c9a0ba2b785f395adc5c7ea037eeaf5d65c
Oakland Evans <oaklandevans@yahoo.com>
2021-08-23 14:04:50 -0500

    data-binding

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   versionString: string = "1.0.0";
+  icon: string = 'assets/images/icons/icons-29.png';
+  logoAlt: string = 'FoodPlate logo';
+  isCurrent: boolean = false;
 
   constructor() { }
 
