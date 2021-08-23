fcae1fff46f448d535c3e68904771a01ac3e40bc
Oakland Evans <oaklandevans@yahoo.com>
2021-08-23 11:59:41 -0500

    Header-Footer-Styling

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  versionString: string = "1.0.0";
+
   constructor() { }
 
   ngOnInit(): void {
